dbaf1e7ab6877a93fff02ffa20e4efd15b21d457
maedayukimi <maedayukimi@icloud.com>
2025-02-24 16:59:48 +0900

    removed test.c, fixed includes norminette error

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,12 +6,12 @@
 /*   By: mawako <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 19:46:19 by mawako            #+#    #+#             */
-/*   Updated: 2025/02/24 12:44:15 by mawako           ###   ########.fr       */
+/*   Updated: 2025/02/24 16:58:37 by mawako           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FRACTOL_H
-# define FRACTOL_H
+#ifndef PIPEX_H
+# define PIPEX_H
 
 # include "../libft/libft.h"
 # include <stdio.h>
@@ -24,6 +24,6 @@
 char	*checkpath(char *cmd, char **env);
 int		px_execve(char *av, char **env);
 void	exit_msg(char *msg);
-void	help_msg();
+void	help_msg(void);
 
 #endif
